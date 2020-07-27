ff4307a0fdf5d38ba62aa8308f45744c6414ba77
Woojin Kim <59465590+hyung1721@users.noreply.github.com>
2020-07-27 15:01:46 +0900

    Add files via upload

---
+++ make_frame.py
@@ -0,0 +1,16 @@
+import cv2
+
+vidcap = cv2.VideoCapture('movie/test1.mp4')
+ 
+count = 0
+ 
+while(vidcap.isOpened()):
+    ret, image = vidcap.read()
+ 
+    if(int(vidcap.get(1)) % 20 == 0):
+        print('Saved frame number : ' + str(int(vidcap.get(1))))
+        cv2.imwrite('image/frame%d.jpg' % count, image)
+        print('Saved frame%d.jpg' % count)
+        count += 1
+
+vidcap.release()
